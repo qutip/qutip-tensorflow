0368049c16e34322fb5a898b2dc29a1694587553
Asier Galicia Martinez <A.GaliciaMartinez@student.tudelft.nl>
2021-06-13 20:37:26 +0200

    Sepparated numpy and tensorflow from qutip benchmarks.
    In this way the code is more clear althoug there is some repetitions
    that could be avoided.

--- tests/test_linear_algebra.py
+++ tests/test_linear_algebra.py
@@ -2,42 +2,67 @@ import time
 import pytest
 import qutip as qt
 import numpy as np
-# import tensorflow as tf
+import tensorflow as tf
 
-# Maybe remove np to simplify code.
-# Tensorflow shows deprecation warning for imp
-@pytest.mark.parametrize(
-    ["operation", "dtype"],
-    [pytest.param(qt.Qobj.__matmul__, qt.data.Dense, id="qt-Dense-matmul"),
-     pytest.param(qt.Qobj.__matmul__, qt.data.CSR, id="qt-CSR-matmul"),
-     pytest.param(np.matmul, qt.data.Dense, id="np-Dense-matmul"),
-     pytest.param(qt.Qobj.__add__, qt.data.Dense, id="qt-Dense-add"),
-     pytest.param(qt.Qobj.__add__, qt.data.CSR, id="qt-CSR-add"),
-     pytest.param(np.add, qt.data.Dense, id="np-Dense-add"),
-     # Disabled as it caused due to imp deprecation error
-     # pytest.param(tf.matmul, qt.data.Dense, id="np-Dense-matmul"),
-    ]
-)
+size_max = 2
+size_n = 2
+
+@pytest.mark.parametrize("dtype", [qt.data.Dense, qt.data.CSR], ids=[
+    "dense", "sparse"])
+@pytest.mark.parametrize("operation", [qt.Qobj.__matmul__, qt.Qobj.__add__], ids=[
+    "matmul", "add"])
 @pytest.mark.parametrize("density", [0.1,1], ids=[ "density=0.1", "density=1"])
-@pytest.mark.parametrize("size", np.logspace(1,2,3, dtype = int).tolist())
-def test_linear_algebra(benchmark, size, operation, density, dtype, request):
-    # Group benchmark by operationa and density
+@pytest.mark.parametrize("size", np.logspace(1,size_max,size_n, dtype = int).tolist())
+def test_linear_algebra_binary_op_qt(benchmark, size, operation, density, dtype, request):
+    # Group benchmark by operation, density and size.
     group = request.node.callspec.id
     group = group.split('-')
-    group = group[-1] + '-' + group[1]
+    group = group[-2] + '-' + group[1] + '-' + group[0]
     benchmark.group = group
-
 
     # Create unitary
     U = qt.rand_unitary(size, density=density,
                        dtype = dtype)
 
-    # If numpy method convert array to narray (__array__ method does not work
-    # well).
-    if operation.__class__ == np.ufunc:
-        U = U.full()
-
     result = benchmark(operation, U, U)
 
     # Ensure results are correct
     assert True
+
+@pytest.mark.parametrize("operation", [np.matmul, np.add], ids=[
+    "matmul", "add"])
+@pytest.mark.parametrize("density", [0.1,1], ids=[ "density=0.1", "density=1"])
+@pytest.mark.parametrize("size", np.logspace(1,size_max,size_n, dtype = int).tolist())
+def test_linear_algebra_np(benchmark, size, operation, density, request):
+    # Group benchmark by operationa, density and size.
+    group = request.node.callspec.id
+    group = group.split('-')
+    group = group[-1] + '-' + group[1] + '-' + group[0]
+    benchmark.group = group
+
+    # Create unitary
+    U = qt.rand_unitary(size, density=density).full()
+
+    result = benchmark(operation, U, U)
+
+    # Ensure results are correct
+    assert True # TODO
+
+@pytest.mark.parametrize("operation", [tf.matmul, tf.add], ids=[
+    "matmul", "add"])
+@pytest.mark.parametrize("density", [0.1,1], ids=[ "density=0.1", "density=1"])
+@pytest.mark.parametrize("size", np.logspace(1,2,size_n, dtype = int).tolist())
+def test_linear_algebra_np(benchmark, size, operation, density, request):
+    # Group benchmark by operationa, density and size.
+    group = request.node.callspec.id
+    group = group.split('-')
+    group = group[-1] + '-' + group[1] + '-' + group[0]
+    benchmark.group = group
+
+    # Create unitary
+    U = qt.rand_unitary(size, density=density).full()
+
+    result = benchmark(operation, U, U)
+
+    # Ensure results are correct
+    assert True # TODO
