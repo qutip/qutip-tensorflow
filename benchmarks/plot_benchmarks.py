5f4f9622fe53023c4e6fb26cc64652638aa37cdf
Asier Galicia <A.GaliciaMartinez@student.tudelft.nl>
2021-06-17 18:30:23 +0200

    Moved the workflow to functions.

--- benchmarks/plot_benchmarks.py
+++ benchmarks/plot_benchmarks.py
@@ -4,9 +4,6 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# with open('../bench') as f:
-    # print(f)
-
 def unravel(data, key):
     for d in data:
         values = d.pop(key)
@@ -14,7 +11,7 @@ def unravel(data, key):
             d[key+'_'+k] = v
     return data
 
-def benchmark_to_datafrane(filepath):
+def benchmark_to_datafrane(filepath, df_save_path):
     path = pathlib.Path(__file__).absolute().parents[1].joinpath(filepath)
     with open(path) as f:
         data = json.load(f)
@@ -26,21 +23,17 @@ def benchmark_to_datafrane(filepath):
         data = pd.DataFrame(data)
 
         # Set operation properly (for example: matmul instead of:
-        # UNSERIALIZABLE[<function Qobj.__matmul__ at 0x...)
+        # UNSERIALIZABLE[<function Qobj.__mammal__ at 0x...)
         # The name of the operation is obtained from the group name
         data.params_operation = data.group.str.split('-')
         data.params_operation = [d[0] for d in data.params_operation]
 
+        data.to_csv(df_save_path)
+
         return data
 
-
-if __name__ == '__main__':
-    filepath = ".benchmarks/Linux-CPython-3.8-64bit/0029_05d4e025622c1975caabbb591b8ff35555cf830e_20210613_200950_uncommited-changes.json"
-    df = benchmark_to_datafrane(filepath)
-
-    save_path = filepath[:-4]+'csv'
-    df.to_csv(save_path)
-
+def plot_benchmark(df_path, destination_folder):
+    df = pd.read_csv(df_path, index_col=0)
     grouped = df.groupby([ 'params_operation', 'params_density'])
     for (operation,density), group in grouped:
         for dtype,g in group.groupby('extra_info_dtype'):
@@ -52,4 +45,15 @@ if __name__ == '__main__':
         plt.savefig(f".benchmarks/figures/{operation}_density={density}.png")
         plt.close()
 
+def run_benchmarks():
+    pass
 
+
+if __name__ == '__main__':
+    filepath = ".benchmarks/Linux-CPython-3.8-64bit/0001_591960324f222f0d7dd1c471203d2347b8f28b52_20210617_160553_uncommited-changes.json"
+    df_save_path = ".benchmarks/Linux-CPython-3.8-64bit/latest.csv"
+    figures_save_path = ".benchmarks/figures/"
+
+    run_benchmarks()
+    benchmark_to_datafrane(filepath, df_save_path)
+    plot_benchmark(df_save_path, figures_save_path)
