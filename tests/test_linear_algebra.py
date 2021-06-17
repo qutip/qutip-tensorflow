97d480996fca3e2eda60b0fd960c1d4940edb41d
Asier Galicia <A.GaliciaMartinez@student.tudelft.nl>
2021-06-17 18:37:17 +0200

    Merged tensorflows test with numpy.

--- tests/test_linear_algebra.py
+++ tests/test_linear_algebra.py
@@ -29,16 +29,23 @@ def test_linear_algebra_qt(benchmark, size, operation, density, dtype, request):
     # Ensure results are correct
     assert True
 
-@pytest.mark.parametrize("dtype", [np], ids=["numpy"])
-@pytest.mark.parametrize("operation", [np.matmul, np.add], ids=["matmul", "add"])
+@pytest.mark.parametrize("dtype", [np, tf], ids=["numpy", "tensorflow"])
+@pytest.mark.parametrize("operation", ["matmul", "add"], ids=["matmul", "add"])
 @pytest.mark.parametrize("density", [0.1,1], ids=[ "density=0.1", "density=1"])
 @pytest.mark.parametrize("size", np.logspace(1,size_max,size_n, base=2, dtype = int).tolist())
-def test_linear_algebra_np(benchmark, dtype, size, operation, density, request):
+def test_linear_algebra_ex(benchmark, dtype, size, operation, density, request):
     # Group benchmark by operationa, density and size.
     group = request.node.callspec.id
     group = group.split('-')
     benchmark.group = group[-2] + '-' + group[1] + '-' + group[0]
     benchmark.extra_info['dtype'] = group[-1]
+
+    # Get operation
+    if operation=="matmul":
+        operation = dtype.matmul
+
+    if operation=="add":
+        operation = dtype.add
 
     # Create unitary
     U = qt.rand_unitary(size, density=density).full()
@@ -48,21 +55,3 @@ def test_linear_algebra_np(benchmark, dtype, size, operation, density, request):
     # Ensure results are correct
     assert True # TODO
 
-@pytest.mark.parametrize("dtype", [tf], ids=["tensorflow"])
-@pytest.mark.parametrize("operation", [tf.matmul, tf.add], ids=["matmul", "add"])
-@pytest.mark.parametrize("density", [0.1,1], ids=[ "density=0.1", "density=1"])
-@pytest.mark.parametrize("size", np.logspace(1,size_max,size_n, base=2, dtype = int).tolist())
-def test_linear_algebra_tf(benchmark, dtype, size, operation, density, request):
-    # Group benchmark by operationa, density and size.
-    group = request.node.callspec.id
-    group = group.split('-')
-    benchmark.group = group[-2] + '-' + group[1] + '-' + group[0]
-    benchmark.extra_info['dtype'] = group[-1]
-
-    # Create unitary
-    U = qt.rand_unitary(size, density=density).full()
-
-    result = benchmark(operation, U, U)
-
-    # Ensure results are correct
-    assert True # TODO
