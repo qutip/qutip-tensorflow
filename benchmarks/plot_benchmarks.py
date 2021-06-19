42682315678b052fd3229ae463718dd78de1ca52
Asier Galicia <A.GaliciaMartinez@student.tudelft.nl>
2021-06-19 14:57:39 +0200

    plot_benchmarks now runs benchmarks and acepts arguments.
    The accepted arguments are those for pytest at this moment.

--- benchmarks/plot_benchmarks.py
+++ benchmarks/plot_benchmarks.py
@@ -3,6 +3,8 @@ import pathlib
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+import pytest
+import argparse
 
 def unravel(data, key):
     for d in data:
@@ -38,6 +40,7 @@ def plot_benchmark(df_path, destination_folder):
     for (operation,density), group in grouped:
         for dtype,g in group.groupby('extra_info_dtype'):
             plt.errorbar(g.params_size, g.stats_mean, g.stats_stddev, fmt='.-', label=dtype)
+
         plt.title(operation + str(density))
         plt.legend()
         plt.yscale('log')
@@ -45,15 +48,21 @@ def plot_benchmark(df_path, destination_folder):
         plt.savefig(f".benchmarks/figures/{operation}_density={density}.png")
         plt.close()
 
-def run_benchmarks():
-    pass
+def run_benchmarks(args):
+    pytest.main(["--benchmark-only",
+                 "--benchmark-columns=Mean,StdDev",
+                 "--benchmark-sort=name"] +
+               args)
 
 
 if __name__ == '__main__':
-    filepath = ".benchmarks/Linux-CPython-3.8-64bit/0001_591960324f222f0d7dd1c471203d2347b8f28b52_20210617_160553_uncommited-changes.json"
+    filepath = ".benchmarks/Linux-CPython-3.8-64bit/0031_f6c63e6c1b95a648627a1cc6382f27c3a0181c78_20210618_142829_uncommited-changes.json"
     df_save_path = ".benchmarks/Linux-CPython-3.8-64bit/latest.csv"
     figures_save_path = ".benchmarks/figures/"
 
-    run_benchmarks()
+    parser = argparse.ArgumentParser()
+    args, other_args = parser.parse_known_args()
+
+    run_benchmarks(other_args)
     benchmark_to_datafrane(filepath, df_save_path)
     plot_benchmark(df_save_path, figures_save_path)
