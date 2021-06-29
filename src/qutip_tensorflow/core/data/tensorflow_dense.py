Added test and expections to catch wrong shapes.

src/qutip_tensorflow/core/data/tensorflow_dense.py
@@ -33,6 +33,7 @@ class DenseTensor(data.Data):
             and isinstance(shape[1], numbers.Integral)
             and shape[0] > 0
             and shape[1] > 0
+            and isinstance(shape, tuple)
         ):
             raise ValueError("shape must be a 2-tuple of positive ints, but is " + repr(shape))
 
@@ -41,7 +42,11 @@ class DenseTensor(data.Data):
         # if not copy and isinstance(data, tf.Tensor):
             # self._tf = tf
 
-        self._tf = tf.constant(data, shape=shape)
+        try:
+            self._tf = tf.constant(data, shape=shape)
+        except TypeError as e:
+            raise ValueError("Shape of data must match shape argument.") from e
+
         self._tf = tf.cast(self._tf, tf.complex128)
 
         if copy:

tests/core/data/test_tensordense.py
@@ -29,6 +29,10 @@ def _valid_numpy():
     # Arbitrary valid numpy array.
     return conftest.random_numpy_dense((5, 5), False)
 
+def _valid_tensor():
+    # Arbitrary valid numpy array.
+    return conftest.random_tensor_dense((5, 5))
+
 
 @pytest.fixture(scope='function')
 def numpy_dense(shape, fortran):
@@ -82,21 +86,21 @@ class TestClassMethods:
         assert np.all(test._tf == tensor)
 
     @pytest.mark.parametrize(['arg', 'kwargs', 'error'], [
-        pytest.param(_valid_numpy(), {'shape': ()}, ValueError,
+        pytest.param(_valid_tensor(), {'shape': ()}, ValueError,
                      id="numpy-shape 0 tuple"),
-        pytest.param(_valid_numpy(), {'shape': (1,)}, ValueError,
+        pytest.param(_valid_tensor(), {'shape': (1,)}, ValueError,
                      id="numpy-shape 1 tuple"),
-        pytest.param(_valid_numpy(), {'shape': (None, None)}, ValueError,
+        pytest.param(_valid_tensor(), {'shape': (None, None)}, ValueError,
                      id="numpy-shape None tuple"),
-        pytest.param(_valid_numpy(), {'shape': [2, 2]}, ValueError,
+        pytest.param(_valid_tensor(), {'shape': [2, 2]}, ValueError,
                      id="numpy-shape list"),
-        pytest.param(_valid_numpy(), {'shape': (1, 2, 3)}, ValueError,
+        pytest.param(_valid_tensor(), {'shape': (1, 2, 3)}, ValueError,
                      id="numpy-shape 3 tuple"),
-        pytest.param(_valid_numpy(), {'shape': (-1, 1)}, ValueError,
+        pytest.param(_valid_tensor(), {'shape': (-1, 1)}, ValueError,
                      id="numpy-negative shape"),
-        pytest.param(_valid_numpy(), {'shape': (-4, -4)}, ValueError,
+        pytest.param(_valid_tensor(), {'shape': (-4, -4)}, ValueError,
                      id="numpy-both negative shape"),
-        pytest.param(_valid_numpy(), {'shape': (1213, 1217)}, ValueError,
+        pytest.param(_valid_tensor(), {'shape': (1213, 1217)}, ValueError,
                      id="numpy-different shape"),
     ])
     def test_init_from_wrong_input(self, arg, kwargs, error):