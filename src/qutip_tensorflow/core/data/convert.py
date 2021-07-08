20908c96922993d6df9f936f3b1b69a67378ff8d
Asier Galicia <A.GaliciaMartinez@student.tudelft.nl>
2021-07-08 16:00:39 +0200

    Added PR changes.

--- src/qutip_tensorflow/core/data/convert.py
+++ src/qutip_tensorflow/core/data/convert.py
@@ -1,4 +1,4 @@
-import qutip as qt
+import qutip
 from .dense_tensor import DenseTensor
 import tensorflow as tf
 
@@ -14,20 +14,15 @@ def _tf_to_dense(tftensor):
 def is_tftensor(data):
     return isinstance(data, tf.Tensor)
 
-# `add_conversions` will register the data layer
-qt.data.to.add_conversions([
+# Register the data layer
+qutip.data.to.add_conversions([
      (DenseTensor, qt.data.Dense, _tf_from_dense),
      (qt.data.Dense, DenseTensor, _tf_to_dense),
 ])
 
-# You can add user friendly name for conversion with `to` or Qobj creation functions:
-qt.data.to.register_aliases(['tftensor'], DenseTensor)
-qt.data.to.register_aliases(['DenseTensor'], DenseTensor)
+# User friendly name for conversion with `to` or Qobj creation functions:
+qutip.data.to.register_aliases(['tftensor', 'DenseTensor'], DenseTensor)
 
-qt.data.create.add_creators([
+qutip.data.create.add_creators([
     (is_tftensor, DenseTensor, 85),
 ])
-
-to = qt.data.to
-create = qt.data.create
-
