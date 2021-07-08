c4cc1ecc9589a4394e11fe6a26e27c4920944973
Asier Galicia <A.GaliciaMartinez@student.tudelft.nl>
2021-07-08 16:11:24 +0200

    Added PR changes.

--- src/qutip_tensorflow/core/data/convert.py
+++ src/qutip_tensorflow/core/data/convert.py
@@ -2,22 +2,20 @@ import qutip
 from .dense_tensor import DenseTensor
 import tensorflow as tf
 
-__all__ = ['to']
-
 # Conversion function
 def _tf_from_dense(dense):
     return DenseTensor(dense.to_array())
 
 def _tf_to_dense(tftensor):
-    return qt.data.Dense(tftensor.to_array(), copy=False)
+    return qutip.data.Dense(tftensor.to_array(), copy=False)
 
 def is_tftensor(data):
     return isinstance(data, tf.Tensor)
 
 # Register the data layer
 qutip.data.to.add_conversions([
-     (DenseTensor, qt.data.Dense, _tf_from_dense),
-     (qt.data.Dense, DenseTensor, _tf_to_dense),
+     (DenseTensor, qutip.data.Dense, _tf_from_dense),
+     (qutip.data.Dense, DenseTensor, _tf_to_dense),
 ])
 
 # User friendly name for conversion with `to` or Qobj creation functions:
