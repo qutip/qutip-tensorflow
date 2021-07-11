Simpliefied tests for wrong shape input.

tests/core/data/test_tensordense.py
@@ -116,69 +116,26 @@ class TestClassMethods:
         tensor = tf.cast(tensor, dtype=tf.complex128)
         assert np.all(test._tf == tensor)
 
-    @pytest.mark.parametrize(['arg', 'kwargs', 'error'], [
-        pytest.param(_valid_numpy(), {'shape': 1}, ValueError,
-                     id="numpy-shape no len obj"),
-        pytest.param(_valid_numpy(), {'shape': ()}, ValueError,
-                     id="numpy-shape 0 tuple"),
-        pytest.param(_valid_numpy(), {'shape': (1,)}, ValueError,
-                     id="numpy-shape 1 tuple"),
-        pytest.param(_valid_numpy(), {'shape': (None, None)}, ValueError,
-                     id="numpy-shape None tuple"),
-        pytest.param(_valid_numpy(), {'shape': [2, 2]}, ValueError,
-                     id="numpy-shape list"),
-        pytest.param(_valid_numpy(), {'shape': (1, 2, 3)}, ValueError,
-                     id="numpy-shape 3 tuple"),
-        pytest.param(_valid_numpy(), {'shape': (-1, 1)}, ValueError,
-                     id="numpy-negative shape"),
-        pytest.param(_valid_numpy(), {'shape': (-4, -4)}, ValueError,
-                     id="numpy-both negative shape"),
-        pytest.param(_valid_numpy(), {'shape': (1213, 1217)}, ValueError,
-                     id="numpy-different shape"),
-        pytest.param(_valid_list(), {'shape': 1}, ValueError,
-                     id="list-shape no len obj"),
-        pytest.param(_valid_list(), {'shape': ()}, ValueError,
-                     id="list-shape 0 tuple"),
-        pytest.param(_valid_list(), {'shape': (1,)}, ValueError,
-                     id="list-shape 1 tuple"),
-        pytest.param(_valid_list(), {'shape': (None, None)}, ValueError,
-                     id="list-shape None tuple"),
-        pytest.param(_valid_list(), {'shape': [2, 2]}, ValueError,
-                     id="list-shape list"),
-        pytest.param(_valid_list(), {'shape': (1, 2, 3)}, ValueError,
-                     id="list-shape 3 tuple"),
-        pytest.param(_valid_list(), {'shape': (-1, 1)}, ValueError,
-                     id="list-negative shape"),
-        pytest.param(_valid_list(), {'shape': (-4, -4)}, ValueError,
-                     id="list-both negative shape"),
-        pytest.param(_valid_list(), {'shape': (1213, 1217)}, ValueError,
-                     id="list-different shape"),
-        pytest.param(_valid_tensor(), {'shape': 1}, ValueError,
-                     id="tensor-shape no len obj"),
-        pytest.param(_valid_tensor(), {'shape': ()}, ValueError,
-                     id="tensor-shape 0 tuple"),
-        pytest.param(_valid_tensor(), {'shape': (1,)}, ValueError,
-                     id="tensor-shape 1 tuple"),
-        pytest.param(_valid_tensor(), {'shape': (None, None)}, ValueError,
-                     id="tensor-shape None tuple"),
-        pytest.param(_valid_tensor(), {'shape': [2, 2]}, ValueError,
-                     id="tensor-shape list"),
-        pytest.param(_valid_tensor(), {'shape': (1, 2, 3)}, ValueError,
-                     id="tensor-shape 3 tuple"),
-        pytest.param(_valid_tensor(), {'shape': (-1, 1)}, ValueError,
-                     id="tensor-negative shape"),
-        pytest.param(_valid_tensor(), {'shape': (-4, -4)}, ValueError,
-                     id="tensor-both negative shape"),
-        pytest.param(_valid_tensor(), {'shape': (1213, 1217)}, ValueError,
-                     id="tensor-different shape"),
+    @pytest.mark.parametrize("data", [_valid_tensor,_valid_list,_valid_numpy],
+                       ids= ["numpy", "list", "tensor"])
+    @pytest.mark.parametrize("shape",[
+        pytest.param(1, id="shape_no_tuple"),
+        pytest.param((), id="shape_0_tuple"),
+        pytest.param((1,), id="shape_1_tuple"),
+        pytest.param((None, None), id="shape_None_tuple"),
+        pytest.param([2,2], id="shape_list"),
+        pytest.param((1, 2, 3), id="shape_3_tuple"),
+        pytest.param((-1, 1), id="negative_shape"),
+        pytest.param((-4, -4), id="both_negative_shape"),
+        pytest.param((1213, 1217), id="differen_shape"),
     ])
-    def test_init_from_wrong_input(self, arg, kwargs, error):
+    def test_init_from_wrong_shape(self, data, shape):
         """
         Test that the __init__ method raises a suitable error when passed
         incorrectly formatted inputs.
         """
-        with pytest.raises(error):
-            DenseTensor(arg, **kwargs)
+        with pytest.raises(ValueError):
+            DenseTensor(data, shape)
 
     def test_copy_returns_a_correct_copy(self, data_tensor_dense):
         """