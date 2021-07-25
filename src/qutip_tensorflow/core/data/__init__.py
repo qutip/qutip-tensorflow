899bde0e4d185bdd32491597ec87f6409543dcd0
Asier Galicia <A.GaliciaMartinez@student.tudelft.nl>
2021-07-25 19:23:29 +0200

    Added kron product.

--- src/qutip_tensorflow/core/data/__init__.py
+++ src/qutip_tensorflow/core/data/__init__.py
@@ -1,3 +1,7 @@
 from .convert import *
 from .tftensor import *
 from .add import *
+
+from .inner import *
+from .trace import *
+from .kron import *
---
+++ src/qutip_tensorflow/core/data/trace.py
@@ -0,0 +1,25 @@
+import qutip
+from .tftensor import TfTensor
+import warnings
+
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
+    import tensorflow as tf
+
+__all__ = ["trace_tftensor"]
+
+
+def trace_tftensor(matrix):
+    if matrix.shape[0] != matrix.shape[1]:
+        raise ValueError(
+            f"""Trace can only be performed in square matrix. This
+                         matrix has shape={matrix.shape}"""
+        )
+    return tf.linalg.trace(matrix._tf)
+
+
+qutip.data.trace.add_specialisations(
+    [
+        (TfTensor, trace_tftensor),
+    ]
+)
